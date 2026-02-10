137e75d68ae0ed6d60cbdd63d3fe67a0c97dd4a6
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-02-10 14:56:01 +0000

    nisra cookie test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { wireSearch } from "./utils/wireSearch.js";
 import { initSidebarPersistence } from "./utils/initSideBarPersistence.js";
 import { share_btn } from "./utils/elements.js";
 import { sharePage } from "./utils/sharePage.js";
+import { initCookieConsent } from "./utils/cookies.js";
 
 let searchIndex = [];
 
@@ -22,6 +23,11 @@ window.addEventListener("DOMContentLoaded", async () => {
     for (let i = 0; i < share_btn.length; i++) {
       share_btn[i].onclick = sharePage;
     }
+
+      initCookieConsent({
+        bannerId: 'cookie-banner',
+        gtmId: 'GTM-KF6WGSG'
+  });
 
   } catch (e) {
     console.error("Startup failed:", e);
